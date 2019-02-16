c7cb4630dbd0d21526ba2543534c3f7f3ada5287
Emmanuel <emmanuel@Emmanuels-MacBook-Pro.local>
2019-02-16 19:25:18 +0800

    broken links fixed

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,9 +1,25 @@
 import project1 from '../assets/myproject1.png';
 import project2 from '../assets/myprojects2.png';
 import project3 from '../assets/myproject3.png';
+import project4 from '../assets/twentek-music2.png'
 
 const PROJECTS = [
     
+  {
+    id: 1,
+    title: 'Twentek Music Master',
+    description: 'Search for your favourite artist, play their songs without any adds',
+    link: 'https://twentek-music-app.netlify.com',
+    image: project4
+  },
+
+  {
+    id: 4,
+    title: 'GithuB User finder',
+    description: 'This application also help you quickly search for any user on github',
+    link: 'https://tweneboah.github.io/github-user-finder',
+    image: project3
+  },
     {
       id: 2,
       title: 'Background Changer app',
@@ -13,19 +29,13 @@ const PROJECTS = [
     },
 
     {
-      id: 1,
+      id: 3,
       title: 'Landing page website',
       description: 'This is a company landing page website',
-      link: 'https://tweneboah.github.io/100-Days-Of-Coding/',
+      link: 'https://tweneboah.github.io/background-gradient-changer/',
       image: project1
     },
-    {
-      id: 3,
-      title: 'GithuB User finder',
-      description: 'This application also help you quickly search for any user on github',
-      link: 'https://tweneboah.github.io/github-user-finder',
-      image: project3
-    }
+    
   ];
   
   export default PROJECTS;
